2a72ff602303e1200c6c997f62e237773a3e981d
seif <116313275+sahnoune1@users.noreply.github.com>
2023-01-28 22:02:14 +0100

    java.js

--- java.js
+++ java.js
@@ -4,10 +4,15 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-const book1 = new Book("harry potter", "jki rowling", 252);
-const book2 = new Book("the lord ", "tolkein", 255);
-console.log(book1);
-console.log(book2);
+const array = [];
 
-const array = [book1, book2];
-console.log(array);
+function addBookToLibrary() {
+  const title = prompt("give me the book title");
+  const author = prompt("give me the author name");
+  const pages = prompt("give me the number of pages");
+
+  const book1 = new Book(title, author, pages);
+  array[0] = book1;
+  return array;
+}
+console.log(addBookToLibrary());
