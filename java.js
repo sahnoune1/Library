b3edd470f394cc34764895a61f235c10ed01b448
seif <116313275+sahnoune1@users.noreply.github.com>
2023-01-29 22:05:10 +0100

    java.js

--- java.js
+++ java.js
@@ -15,8 +15,74 @@ function addBookToLibrary() {
   array.push(book);
   return array;
 }
-let i = 0;
-for (i = 0; i < 2; i++) {
-  addBookToLibrary();
-}
-console.log(array);
+
+const butt = document.createElement("button");
+butt.innerHTML = "give me the books details";
+butt.setAttribute("class", "butt");
+document.body.appendChild(butt);
+
+const form = document.createElement("form");
+
+const titleLabel = document.createElement("label");
+titleLabel.setAttribute("for", "title");
+titleLabel.innerHTML = "title";
+
+const titleInput = document.createElement("input");
+titleInput.setAttribute("type", "text");
+titleInput.setAttribute("id", "title");
+titleInput.setAttribute("name", "title");
+
+form.appendChild(titleLabel);
+form.appendChild(titleInput);
+
+const authorLabel = document.createElement("label");
+authorLabel.setAttribute("for", "author");
+authorLabel.innerHTML = "author";
+
+const authorInput = document.createElement("input");
+authorInput.setAttribute("type", "text");
+authorInput.setAttribute("id", "author");
+authorInput.setAttribute("name", "author");
+
+form.appendChild(authorLabel);
+form.appendChild(authorInput);
+
+const pagesLabel = document.createElement("label");
+pagesLabel.setAttribute("for", "pages");
+pagesLabel.innerHTML = "pages";
+
+const pagesInput = document.createElement("input");
+pagesInput.setAttribute("type", "number");
+pagesInput.setAttribute("id", "pages");
+pagesInput.setAttribute("name", "pages");
+
+form.appendChild(pagesLabel);
+form.appendChild(pagesInput);
+
+const subButtLabel = document.createElement("label");
+subButtLabel.setAttribute("for", "submit");
+
+const subButtInput = document.createElement("input");
+subButtInput.setAttribute("type", "submit");
+subButtInput.setAttribute("id", "submit");
+subButtInput.setAttribute("name", "submit");
+
+form.appendChild(subButtInput);
+form.appendChild(subButtLabel);
+
+document.body.appendChild(form);
+
+butt.addEventListener("click", () => {
+  let i = 0;
+  for (i = 0; i < 2; i++) {
+    addBookToLibrary();
+  }
+  let j = 0;
+  for (j = 0; j < array.length; j++) {
+    const div = document.createElement("div");
+    div.innerHTML = `${array[j].title},${array[j].author},${array[j].pages}`;
+    div.setAttribute("class", "new");
+    document.body.appendChild(div);
+  }
+  console.log(array);
+});
