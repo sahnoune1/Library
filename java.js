8c1e5254603e888e6494f871b8de13e82c6b6dbb
seif <116313275+sahnoune1@users.noreply.github.com>
2023-01-30 21:38:41 +0100

    index.html

--- java.js
+++ java.js
@@ -1,102 +1,144 @@
-// function Book(title, author, pages) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-// }
+const array = [];
+/* function of deleting */
+function renderBooks() {
+  const delet = document.querySelectorAll(".new");
+  let x = 0;
+  for (x = 0; x < delet.length; x++) {
+    delet[x].parentNode.removeChild(delet[x]);
+  }
+  for (let j = 0; j < array.length; j++) {
+    const div = document.createElement("div");
+    div.innerHTML = `${array[j].title},${array[j].author},${array[j].pages}<button class="delete">delete</button>`;
+    div.setAttribute("class", "new");
+    document.body.appendChild(div);
 
-// const array = [];
+    const deleteBtn = div.querySelector(".delete");
+    deleteBtn.addEventListener("click", () => {
+      const index = j;
+      div.parentNode.removeChild(div);
+      array.splice(index, 1);
+      renderBooks();
+    });
+  }
+}
 
-// function addBookToLibrary() {
-//   const title = prompt("give me the book title");
-//   const author = prompt("give me the author name");
-//   const pages = prompt("give me the number of pages");
+/* the book object */
 
-//   const book = new Book(title, author, pages);
-//   array.push(book);
-//   return array;
-// }
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
 
+let form;
+
+let statement = false;
+
+/* button creation */
 const butt = document.createElement("button");
 butt.innerHTML = "give me the books details";
 butt.setAttribute("class", "butt");
 document.body.appendChild(butt);
+
+/* the butt event listener */
+
 butt.addEventListener("click", () => {
-  const form = document.createElement("form");
-
-  const titleLabel = document.createElement("label");
-  titleLabel.setAttribute("for", "title");
-  titleLabel.innerHTML = "title";
-
-  const titleInput = document.createElement("input");
-  titleInput.setAttribute("type", "text");
-  titleInput.setAttribute("id", "title");
-  titleInput.setAttribute("name", "title");
-
-  form.appendChild(titleLabel);
-  form.appendChild(titleInput);
-
-  const authorLabel = document.createElement("label");
-  authorLabel.setAttribute("for", "author");
-  authorLabel.innerHTML = "author";
-
-  const authorInput = document.createElement("input");
-  authorInput.setAttribute("type", "text");
-  authorInput.setAttribute("id", "author");
-  authorInput.setAttribute("name", "author");
-
-  form.appendChild(authorLabel);
-  form.appendChild(authorInput);
-
-  const pagesLabel = document.createElement("label");
-  pagesLabel.setAttribute("for", "pages");
-  pagesLabel.innerHTML = "pages";
-
-  const pagesInput = document.createElement("input");
-  pagesInput.setAttribute("type", "number");
-  pagesInput.setAttribute("id", "pages");
-  pagesInput.setAttribute("name", "pages");
-
-  form.appendChild(pagesLabel);
-  form.appendChild(pagesInput);
-
-  const subButtLabel = document.createElement("label");
-  subButtLabel.setAttribute("for", "submit");
-
-  const subButtInput = document.createElement("input");
-  subButtInput.setAttribute("type", "submit");
-  subButtInput.setAttribute("id", "submit");
-  subButtInput.setAttribute("name", "submit");
-
-  form.appendChild(subButtInput);
-  form.appendChild(subButtLabel);
-
-  document.body.appendChild(form);
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const bookTitle = titleInput.value;
-    const bookAuthor = authorInput.value;
-    const bookPages = pagesInput.value;
-
-    const div = document.createElement("div");
-    div.innerHTML = `${bookTitle},${bookAuthor},${bookPages}`;
-    document.body.appendChild(div);
+  if (statement === true) {
     document.body.removeChild(form);
-  });
-});
+    statement = false;
+  } else if (statement === false) {
+    form = document.createElement("form");
+    const titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    titleLabel.innerHTML = "title";
 
-// butt.addEventListener("click", () => {
-//   let i = 0;
-//   for (i = 0; i < 2; i++) {
-//     addBookToLibrary();
-//   }
-//   let j = 0;
-//   for (j = 0; j < array.length; j++) {
-//     const div = document.createElement("div");
-//     div.innerHTML = `${array[j].title},${array[j].author},${array[j].pages}`;
-//     div.setAttribute("class", "new");
-//     document.body.appendChild(div);
-//   }
-//   console.log(array);
-// });
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    titleInput.setAttribute("name", "title");
+
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+
+    const authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "author");
+    authorLabel.innerHTML = "author";
+
+    const authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("id", "author");
+    authorInput.setAttribute("name", "author");
+
+    form.appendChild(authorLabel);
+    form.appendChild(authorInput);
+
+    const pagesLabel = document.createElement("label");
+    pagesLabel.setAttribute("for", "pages");
+    pagesLabel.innerHTML = "pages";
+
+    const pagesInput = document.createElement("input");
+    pagesInput.setAttribute("type", "number");
+    pagesInput.setAttribute("id", "pages");
+    pagesInput.setAttribute("name", "pages");
+
+    form.appendChild(pagesLabel);
+    form.appendChild(pagesInput);
+
+    const subButtLabel = document.createElement("label");
+    subButtLabel.setAttribute("for", "submit");
+
+    const subButtInput = document.createElement("input");
+    subButtInput.setAttribute("type", "submit");
+    subButtInput.setAttribute("id", "submit");
+    subButtInput.setAttribute("name", "submit");
+
+    form.appendChild(subButtInput);
+    form.appendChild(subButtLabel);
+
+    document.body.appendChild(form);
+
+    /* submit event listener */
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const bookTitle = titleInput.value;
+      const bookAuthor = authorInput.value;
+      const bookPages = pagesInput.value;
+
+      const book = new Book(bookTitle, bookAuthor, bookPages);
+
+      array.push(book);
+
+      renderBooks();
+
+      // function renderBooks() {
+      //   const delet = document.querySelectorAll(".new");
+      //   let x = 0;
+      //   for (x = 0; x < delet.length; x++) {
+      //     delet[x].parentNode.removeChild(delet[x]);
+      //   }
+      //   for (let j = 0; j < array.length; j++) {
+      //     const div = document.createElement("div");
+      //     div.innerHTML = `${array[j].title},${array[j].author},${array[j].pages}<button class="delete">delete</button>`;
+      //     div.setAttribute("class", "new");
+      //     document.body.appendChild(div);
+
+      //     const deleteBtn = div.querySelector(".delete");
+      //     deleteBtn.addEventListener("click", () => {
+      //       const index = j;
+      //       div.parentNode.removeChild(div);
+      //       array.splice(index, 1);
+      //       renderBooks();
+      //     });
+      //   }
+      // }
+
+      titleInput.value = "";
+      authorInput.value = "";
+      pagesInput.value = "";
+      console.log(array);
+    });
+
+    statement = true;
+  }
+});
